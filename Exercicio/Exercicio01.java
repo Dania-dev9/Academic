d2f1e055336f3d34f77419cb65e92054b3659475
Dania-dev9 <daniasadallah@sempreceub.com>
2024-10-12 16:59:54 -0300

    agregar

---
+++ Exercicio/Exercicio01.java
@@ -0,0 +1,6 @@
+public class Exercicio01 {
+	public static void main(String[] args) {
+	System.out.print("Hello world");
+	
+	}
+	}
---
+++ Exercicio/src/Exercicio.java
@@ -0,0 +1,9 @@
+
+public class Exercicio {
+	public static void main (String[] args) {
+		
+	// Declarar as variaveis
+		
+		
+	}
+}
